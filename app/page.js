583a7e371a262f1bc431b209024fc75a86344f4b
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-02-02 18:45:24 +0400

    Fix initial category string

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   const [clicked, setClicked] = useState(false);
   const [count, setCount] = useState(0);
   const [formData, setFormData] = useState({
-    category: "2bdFPJUvFo",
+    category: "",
     playerCount: 1,
     spend: 25,
   })
